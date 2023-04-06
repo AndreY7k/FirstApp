e388f10a3afb67ea9b478df77b1d6f7ff908da55
Andrei.K <kavardakovandrei01@icloud.com>
2023-04-06 19:09:22 +0900

    Добавьте файлы проекта.

---
+++ ConsoleAppM3/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleAppM3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Andrei";
+            Console.WriteLine(MyName);
+            Console.ReadKey();
+        }
+    }
+}
