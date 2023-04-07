3acf22621abb8737099dd910c94f5876896f6895
Andrei.K <kavardakovandrei01@icloud.com>
2023-04-07 22:46:38 +0900

    Добавлена анкета

--- ConsoleAppM3/Program.cs
+++ ConsoleAppM3/Program.cs
@@ -12,6 +12,17 @@
             Console.WriteLine("\t Мне 27 лет");
             Console.WriteLine("\t My name is \n Andrei");
             Console.WriteLine("\x23");
+
+            string name = "Andrei";
+            byte MyAge = 23;
+            bool HaveIApet = true;
+            double MyShoeSize = 44.5;
+
+            Console.WriteLine("My name is {0}", name);
+            Console.WriteLine("MyAge " + MyAge);
+            Console.WriteLine("Do i have a pet " + HaveIApet);
+            Console.WriteLine("My shoe size is " + MyShoeSize);
+
             Console.ReadKey();
         }
     }
